f7425f240bc734bf5c4520bcfe10ac23d6a72dd4
franklyn86 <franklyncabrera@hotmail.com>
2017-05-19 14:50:28 +0200

    test

---
+++ banksies/part1/introToModules/howdy5.js
@@ -0,0 +1,11 @@
+var howdy = {};
+
+howdy.sayIt = function () {
+	console.log('howdy');
+};
+
+module.exports = howdy;
+
+var turtle = 5;
+
+// it's good practice, but not necessary, to put it at the end
